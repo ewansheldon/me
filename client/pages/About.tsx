cf743f8a9a00e158062a65884cc8c67ff1744362
Ewan Sheldon <ewansheldon@gmail.com>
2025-10-20 21:41:03 +0200

    remove test change

--- client/pages/About.tsx
+++ client/pages/About.tsx
@@ -2,7 +2,6 @@ const About = () => {
   return (
     <div className="about-me">
       <h1>Ewan Sheldon</h1>
-      <p>test change</p>
       <p>I am a senior software engineer, with 8 years experience.</p>
       <p>I have worked in both young start-ups and large consultancies, with enormous US health tech companies, family-like cloud-based WiFi management platforms and Web3 DAOs.</p>
       <p>To describe my responsibilities in some projects I have worked on: I have written APIs and other background servers in Node.js and Java, worked on React and Angular frontend apps, designed CI/CD processes deploying to Google Cloud Platforms, work with SQL databases and Redis, written and deployed multiple Solidity smart contracts to EVM compatible blockchains.</p>
