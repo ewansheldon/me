f90e9a72ee14a7290e7cd7b72805b6016f0ad616
Ewan Sheldon <ewansheldon@gmail.com>
2025-10-29 16:50:57 +0100

    restructure menu with selector icon

--- client/pages/Home/Home.tsx
+++ client/pages/Home/Home.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <ul role="menu">
-        <li role="menuitem"><Link to="/cv">Play Movie</Link></li>
-        <li role="menuitem"><Link to="/about">About</Link></li>
-        <li role="menuitem"><Link to="/contact">Contact</Link></li>
+      <ul role="menu" className="menu">
+        <li role="menuitem"><Link to="/cv" className="menu-item">Play Movie</Link><span className="focused-menu-item">&#9642;</span></li>
+        <li role="menuitem"><Link to="/about" className="menu-item">About</Link><span className="focused-menu-item">&#9642;</span></li>
+        <li role="menuitem"><Link to="/contact" className="menu-item">Contact</Link><span className="focused-menu-item">&#9642;</span></li>
       </ul>
     </>
   )
