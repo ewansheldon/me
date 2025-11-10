3ce307a13db7c8bbeb5eba010ffcd1a7c6e3cadd
Ewan Sheldon <ewansheldon@gmail.com>
2025-11-10 16:15:10 +0100

    refactor menu

--- client/pages/Home/Home.tsx
+++ client/pages/Home/Home.tsx
@@ -1,12 +1,57 @@
+import { ReactElement, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+interface MenuItem {
+  label: string;
+  url: string;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    label: 'Play Movie',
+    url: '/cv',
+  },
+  {
+    label: 'About',
+    url: '/about',
+  },
+  {
+    label: 'Contact',
+    url: '/contact',
+  }
+]
+
 const Home = () => {
+  const [ selected, setSelected ] = useState(0);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      const affirmativeKeys = [ ' ', 'Enter' ];
+      if (affirmativeKeys.includes(e.key)) {
+        console.log(menuItems[selected].url)
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [selected]);
+
+  const menuItemLi = (menuItem: MenuItem, index: number): ReactElement => {
+    return (
+      <li key={index} role="menuitem">
+        {index === selected && <span className="focused-menu-item">&#9642;</span>}
+        <Link to={menuItem.url} onMouseOver={() => setSelected(index)} className="menu-item">{menuItem.label}</Link>
+      </li>
+    );
+  }
+
   return (
     <>
       <ul role="menu" className="menu">
-        <li role="menuitem"><Link to="/cv" className="menu-item">Play Movie</Link><span className="focused-menu-item">&#9642;</span></li>
-        <li role="menuitem"><Link to="/about" className="menu-item">About</Link><span className="focused-menu-item">&#9642;</span></li>
-        <li role="menuitem"><Link to="/contact" className="menu-item">Contact</Link><span className="focused-menu-item">&#9642;</span></li>
+        { menuItems.map((menuItem, i) => menuItemLi(menuItem, i)) }
       </ul>
     </>
   )
